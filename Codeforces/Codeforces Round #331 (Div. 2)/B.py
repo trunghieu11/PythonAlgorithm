81095e6bfc53c2582fd2080da5c102c4a8e3c627
hieunt6 <hieunt6@vng.com.vn>
2015-11-20 14:24:15 +0700

    From VNG 20/11/2015

--- Codeforces/Codeforces Round #331 (Div. 2)/B.py
+++ Codeforces/Codeforces Round #331 (Div. 2)/B.py
@@ -1,1 +1,16 @@
 __author__ = 'trunghieu11'
+
+
+def solve(n, A):
+    answer = 0
+    cur = 0
+    for x in A:
+        answer += abs(x - cur)
+        cur = x
+    return answer
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    A = map(int, raw_input().split(" "))
+    print solve(n, A)
--- Codeforces/Educational Codeforces Round 1/A.py
+++ Codeforces/Educational Codeforces Round 1/A.py
@@ -1,1 +1,16 @@
 __author__ = 'trunghieu11'
+
+
+def solve(n):
+    total = n * (n + 1) / 2
+    twoMinus = 1
+    while twoMinus <= n:
+        twoMinus *= 2
+    return total - 2 * (twoMinus - 1)
+
+
+if __name__ == '__main__':
+    testCase = int(raw_input())
+    for test in range(testCase):
+        n = int(raw_input())
+        print solve(n)
